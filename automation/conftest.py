3c77532f76a3d82ea3f840020c51767a62fe5e58
exal-rom <131982956+exal-rom@users.noreply.github.com>
2026-05-22 14:26:58 +0200

    DOC: Update conftest.py

--- automation/conftest.py
+++ automation/conftest.py
@@ -1,1 +1,16 @@
-change xontent
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+BASE_URL = "https://qae-assignment-tau.vercel.app/?user-id=candidate-rmJCQj360K"
+
+@pytest.fixture
+def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get(BASE_URL)
+    yield driver
+    driver.quit()
+
